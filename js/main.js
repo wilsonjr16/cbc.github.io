c7579360c72121fa270f23564d9797208d676024
Isabela Silveira <isabelatsilveira@gmail.com>
2017-10-28 16:10:11 -0300

    Mudanças na home

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 jQuery(document).ready( function(){
 
-	jQuery('.hamburguer').click(function(){
-	jQuery('.mobile_header, .nav_mobile_wrapper').addClass('active');
-	jQuery('.blackout2').fadeIn();
-	jQuery('body').addClass('scroll-hidden');
-	});
-
-	jQuery('.menu_close').click(function(){
-		jQuery('.mobile_header, .nav_mobile_wrapper').removeClass('active');
-		jQuery('.blackout2').fadeOut();
-		jQuery('body').removeClass('scroll-hidden');
-	});
-
-
-
-  jQuery("#owl-painel").owlCarousel({
-      dots: false,
-      nav: true,
-      items : 1,
-      autoplay: true,
-      loop: true,
-      lazyLoad:true,
-      responsiveClass: true
+  jQuery('.hamburguer').click(function(){
+  jQuery('.mobile_header, .nav_mobile_wrapper').addClass('active');
+  jQuery('.blackout2').fadeIn();
+  jQuery('body').addClass('scroll-hidden');
   });
+
+  jQuery('.menu_close').click(function(){
+      jQuery('.mobile_header, .nav_mobile_wrapper').removeClass('active');
+      jQuery('.blackout2').fadeOut();
+      jQuery('body').removeClass('scroll-hidden');
+  });
+
+ var e=new Instafeed({
+template:'<li class="col-sm-2 insta_item"><a href="{{link}}" title="Instagram" class="insta-link"><img src="{{image}}" class="img-responsive center-block"></a></li>',
+get:"user",
+userId:3281923690,
+accessToken:"3281923690.7c61f5a.cec3aaea1a054ffda79af80434bd5963",
+limit:6,
+resolution:"low_resolution"});
+e.run();
+
+jQuery("#owl-painel").owlCarousel({
+  dots: false,
+  nav: true,
+  items : 1,
+  autoplay: true,
+  loop: true,
+  lazyLoad:true,
+  responsiveClass: true
+});
+  
+                       
 });
